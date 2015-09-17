d342bca53b377dfd4f0182d626d1fe4d4d30f7c3
Maurice de Beijer <mauricedb@computer.org>
2015-09-17 10:41:52 +0100

    Execute test on changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@
  */
 
 var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 
+gulp.task('mocha', function () {
+    return gulp.src('tests/**/*.js', {read: false})
+        .pipe(mocha());
+});
 
-gulp.task('default', []);
+gulp.task('watch', function () {
+    gulp.watch('**/*.js', ['mocha']);
+});
+
+gulp.task('default', ['mocha', 'watch']);
