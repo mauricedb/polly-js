c1dc3e02e8cd0b6edae3b77becd30230cefb278b
Maurice de Beijer <maurice.de.beijer@gmail.com>
2015-12-11 16:18:29 +0100

    Add TS file

---
+++ d.ts/polly-js.d.ts
@@ -0,0 +1,20 @@
+declare interface AsyncRetryable {
+    executeForPromise(fn: Function): any
+    executeForNode(fn: Function): any
+}
+
+declare interface Retryable extends AsyncRetryable {
+    execute(fn: Function): any
+}
+
+declare interface Polly {
+    handle(fn: Function): Polly
+    retry(): Retryable
+    waitAndRetry(delays): AsyncRetryable
+}
+
+declare var polly: () => Polly;
+
+declare module "polly-js" {
+    export = polly;
+}
