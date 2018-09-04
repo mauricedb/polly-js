7c0162105ed34b17801c6d8ad89f1a455ac5d282
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-09-04 11:31:10 +0200

    Updated type defintion to include execution info parameter

--- d.ts/polly-js.d.ts
+++ d.ts/polly-js.d.ts
@@ -1,21 +1,28 @@
+type Info = {
+    count: number;
+};
+
 declare interface AsyncRetryable {
-    executeForPromise<T>(fn: () => Promise<T>): Promise<T>
-    executeForNode(fn: (err?: object, data?: any) => any, cb?: (err?: object, data?: any) => any): void
+    executeForPromise<T>(fn: (info: Info) => Promise<T>): Promise<T>;
+    executeForNode(
+        fn: (cb: (err?: object, data?: any) => any, info: Info) => any,
+        cb?: (err?: object, data?: any) => any
+    ): void;
 }
 
 declare interface Retryable extends AsyncRetryable {
-    execute(fn: Function): any
+    execute(fn: (info: Info) => any): any;
 }
 
 declare interface Polly {
-    handle(fn: (err: any) => boolean): Polly
-    retry(numRetries: number): Retryable
-    waitAndRetry(delays: number[]): AsyncRetryable
-    waitAndRetry(numRetries : number): AsyncRetryable
+    handle(fn: (err: any) => boolean): Polly;
+    retry(numRetries: number): Retryable;
+    waitAndRetry(delays: number[]): AsyncRetryable;
+    waitAndRetry(numRetries: number): AsyncRetryable;
 }
 
 declare var polly: () => Polly;
 
-declare module "polly-js" {
+declare module 'polly-js' {
     export default polly;
 }
