3be4c9d9789d66dc150569d92f49d1a7d865c6fe
gstamac <1668205+gstamac@users.noreply.github.com>
2019-05-07 12:15:25 +0200

    Fix TS type definition

--- d.ts/polly-js.d.ts
+++ d.ts/polly-js.d.ts
@@ -1,28 +1,39 @@
-type Info = {
-    count: number;
-};
-
-declare interface AsyncRetryable {
-    executeForPromise<T>(fn: (info: Info) => Promise<T>): Promise<T>;
-    executeForNode(
-        fn: (cb: (err?: object, data?: any) => any, info: Info) => any,
-        cb?: (err?: object, data?: any) => any
-    ): void;
-}
-
-declare interface Retryable extends AsyncRetryable {
-    execute(fn: (info: Info) => any): any;
-}
-
-declare interface Polly {
-    handle(fn: (err: any) => boolean): Polly;
-    retry(numRetries: number): Retryable;
-    waitAndRetry(delays: number[]): AsyncRetryable;
-    waitAndRetry(numRetries: number): AsyncRetryable;
-}
-
-declare var polly: () => Polly;
+// tslint:disable
+// Type definitions for polly-js
 
 declare module 'polly-js' {
-    export default polly;
-}
+    function polly (): polly.Polly;
+
+    namespace polly {
+      type Info = {
+        count: number;
+      };
+
+      interface AsyncRetryable {
+        executeForPromise<T> (fn: (info: Info) => Promise<T>): Promise<T>;
+        executeForNode (
+          fn: (cb: (err?: object, data?: any) => any, info: Info) => any,
+          cb?: (err?: object, data?: any) => any
+        ): void;
+      }
+
+      interface Retryable extends AsyncRetryable {
+        execute (fn: (info: Info) => any): any;
+      }
+
+      interface Polly {
+        handle (fn: (err: any) => boolean): Polly;
+        retry (numRetries: number): Retryable;
+        waitAndRetry (delays: number[]): AsyncRetryable;
+        waitAndRetry (numRetries: number): AsyncRetryable;
+      }
+
+      type PollyDefaults = {
+        delay: number;
+      }
+
+      const defaults: PollyDefaults;
+    }
+
+    export = polly;
+  }
